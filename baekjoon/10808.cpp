bd2c880f96b3b0de651ad530fd8df36f51016bc0
AllveGit <allvegamedevelop@gmail.com>
2023-01-05 01:02:11 +0900

    백준 기존 문제풀이 문제들 주석 포맷 수정

--- baekjoon/10808.cpp
+++ baekjoon/10808.cpp
@@ -1,9 +1,8 @@
-/*
- * 문제 : 일곱 난쟁이
+/**
+ * 문제 제목 : 일곱 난쟁이
+ * 문제 링크 : https://www.acmicpc.net/problem/10808
  *
  * 알파벳 소문자로만 이루어진 단어 S가 주어진다. 각 알파벳이 단어에 몇 개가 포함되어 있는지 구하는 프로그램을 작성하시오.
- * 
- * 문제 링크 : https://www.acmicpc.net/problem/10808
  */
 
 #include <bits/stdc++.h>
--- baekjoon/10988.cpp
+++ baekjoon/10988.cpp
@@ -1,11 +1,10 @@
-/*
- * 문제 : 팰린드롬
+/**
+ * 문제 제목 : 팰린드롬
+ * 문제 링크 : https://www.acmicpc.net/problem/10988
  *
  * 알파벳 소문자로만 이루어진 단어가 주어진다. 이때, 이 단어가 팰린드롬인지 아닌지 확인하는 프로그램을 작성하시오.
  * 팰린드롬이란 앞으로 읽을 때와 거꾸로 읽을 때 똑같은 단어를 말한다. 
  * level, noon은 팰린드롬이고, baekjoon, online, judge는 팰린드롬이 아니다.
- * 
- * 문제 링크 : https://www.acmicpc.net/problem/10988
  */
 
 #include <bits/stdc++.h>
